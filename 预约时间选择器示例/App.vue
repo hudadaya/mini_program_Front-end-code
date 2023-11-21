7453f84ed29100da4434dc001d466d1e5787c4c0
Hu Yin <1140402827@qq.com>
2023-11-22 00:41:44 +0800

    上传预约时间选择

---
+++ 预约时间选择器示例/App.vue
@@ -0,0 +1,11 @@
+<script>
+	 export default {
+	        globalData: {
+	           url: 'xxxx' // 写入公共的请求域名
+	         }
+	    }
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ 预约时间选择器示例/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<!-- 预约时间段的写法 -->
+		<times @change="getTime" :isMultiple="true" :timeInterval="1" :isQuantum="true" :disableTimeSlot="disableTimeSlot"></times>
+		
+		<!-- 多选时间的配置 -->
+		<!-- <times @change="getTime" :timeInterval="1" :appointTime="appointTime"
+		:isMultiple="true" :disableTimeSlot = "disableTimeSlot"></times> -->
+		
+		<!-- 单选的配置 -->
+		<!-- <times @change="getTime" :timeInterval="1" :appointTime="appointTime"
+		:isMultiple="false" :disableTimeSlot = "disableTimeSlot"></times> -->
+		
+		<!-- 预约时间段 -->
+		<!-- <times @change="getTime" :timeInterval="1" :appointTime="appointTime"
+		:isSection="true" :disableTimeSlot = "disableTimeSlot"></times> -->
+	</view>
+</template>
+
+<script>
+	import times from '@/components/pretty-times/pretty-times.vue'
+	export default {
+		components: {
+			times
+		},
+		data() {
+			return {
+				appointTime: ["2022-02-10 15:30:00"],
+				disableTimeSlot: [
+					["2022-10-17 09:00:00", "2022-10-17 10:00:00"],
+					["2022-05-05 16:30:00", "2022-05-05 18:30:00"]
+				]
+			}
+		},
+		methods: {
+			getTime(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
